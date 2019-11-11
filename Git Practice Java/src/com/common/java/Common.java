2e8c48319aa9204a4cdd7bd8c2d3e66f0a8372bb
liorsh <liorsh@shenkar.ac.il>
2019-11-11 13:11:40 +0200

    add fuction

--- Git Practice Java/src/com/common/java/Common.java
+++ Git Practice Java/src/com/common/java/Common.java
@@ -7,4 +7,8 @@ public class Common {
 
 	}
 	
+	public void liorFunction(){
+		
+	}
+	
 }
--- Git Practice Java/src/com/project_lior/java/LiorClass.java
+++ Git Practice Java/src/com/project_lior/java/LiorClass.java
@@ -3,4 +3,6 @@ package com.project_lior.java;
 public class LiorClass {
 	
 	private String hello;
+	private String hi;
+	
 }
